207b95e2ff4accc3d82663d595c7ad9b78e7251f
Dmitrii Pochtennov <dmitrii.pochtennov@gmail.com>
2020-04-24 18:50:38 +0200

    make publish error fixed

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,4 +3,7 @@
 const { program } = require('commander');
 program
     .version('1.0.0')
-    .description('Compares two configuration files and shows a difference.');
+    .description('Compares two configuration files and shows a difference.')
+    .arguments('<firstConfig> <secondConfig>')
+    .option('-f, --format [type]', 'output format', 'json')
+    .parse(process.argv);
